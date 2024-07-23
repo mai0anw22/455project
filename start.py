ff5f386cdebc229a0dfc9412df632b40e9f0511a
mai0anw22 <143766820+mai0anw22@users.noreply.github.com>
2024-07-23 19:30:40 -0400

    first commit

---
+++ start.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchaudio import datasets
+import pandas as pd
+import numpy as np
+import soundata
+
+##importations done
+
+#prepare datasets
+
+dataset = soundata.initialize("urbansound8k")
+dataset.download() #download the dataset
+dataset.validate() #validate the expected files are present
+
+
+
+
+## define a basic model
