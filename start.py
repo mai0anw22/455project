91e29ce5c19c06a95552567a2e69ff2de840c157
mai0anw22 <143766820+mai0anw22@users.noreply.github.com>
2024-07-23 20:38:13 -0400

    testting

--- start.py
+++ start.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 from torchaudio import datasets
 import pandas as pd
 import numpy as np
+import matplotlib as plt
 import soundata
 
 ##importations done
@@ -15,6 +16,7 @@ dataset.download() #download the dataset
 dataset.validate() #validate the expected files are present
 
 #test
+#test 2
 
 
 ## define a basic model
